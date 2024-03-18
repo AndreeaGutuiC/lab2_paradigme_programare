bd6f02048718f2c2d4929e9ad4703c239ff15d24
AndreeaGutuiC <andreeacarolina.gutui@ulbsibiu.ro>
2024-03-18 21:58:19 +0200

    Problema 2. Clasele DoubleChainedNode si .DoubleChainedList'

--- src/ro/ulbs/paradigme/lab2/doublechained/DoubleChainedList.java
+++ src/ro/ulbs/paradigme/lab2/doublechained/DoubleChainedList.java
@@ -1,4 +1,14 @@
 package ro.ulbs.paradigme.lab2.doublechained;
 
 public class DoubleChainedList {
+    private DoubleChainedNode headNode;
+    private DoubleChainedNode tailNode;
+    private int size;
+    private int modCount;
+
+    public DoubleChainedList() {
+        this.headNode = null;
+        this.tailNode = null;
+        this.size = 0;
+    }
 }
